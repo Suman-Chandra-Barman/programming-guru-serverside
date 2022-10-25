c833edecd78b32923f76b3dfb7fde6b4f08b4312
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-10-25 22:22:35 +0600

    reate api and fake data

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const courses = require("./data/courses.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Programming Guru Server is Running...");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id == id);
+  res.send(course);
 });
 
 app.listen(port, () => {
